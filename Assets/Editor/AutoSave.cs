1b136ef500b100bebaa0bc74bb98687c56af9069
Vitor Hugo <vitorhdepaula1@gmail.com>
2019-08-02 17:07:35 -0300

    first commit

---
+++ Assets/Editor/AutoSave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class AutosaveOnRun
+{
+	static AutosaveOnRun()
+	{
+		EditorApplication.playmodeStateChanged = () =>
+		{
+			if(EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+			{
+				Debug.Log("Auto-Saving scene before entering Play mode: " + EditorApplication.currentScene);
+                  
+				EditorApplication.SaveScene();
+				AssetDatabase.SaveAssets();
+			}
+		};
+	}
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.Experimental.UIElements.Image;
+using Object = UnityEngine.Object;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
